54d6987d7d6c195f9ec49553f30198f12a0a02b0
Nokskiy <tsfkafsu@yandex.ru>
2025-08-18 17:51:36 +0500

    logger update

--- src/cleaner.h
+++ src/cleaner.h
@@ -12,6 +12,15 @@ static inline int clean_dir(const char* path)
 {
     DIR* dir = opendir(path);
 
+    if (dir == NULL)
+    {
+        char* mess = concat("directory with name", path);
+        mess = concat(mess,"not exists");
+        log_error(mess);
+        free(mess);
+    }
+
+
     struct dirent* entry;
 
     rewinddir(dir);
@@ -19,24 +28,10 @@ static inline int clean_dir(const char* path)
     {
         if (strcmp(entry->d_name,".") == 0 || strcmp(entry->d_name,"..") == 0) continue;
 
-        char* fp = full_path(path, entry->d_name);
+        char* fp = concat(path, entry->d_name);
         log_info(fp);
         free(fp);
     }
 
     return 0;
-}
-
-inline char* full_path(const char* path1, const char* path2)
-{
-    const size_t len1 = strlen(path1);
-    const size_t len2 = strlen(path2);
-
-    char* result = malloc(len1 + len2 + 1);
-    if (!result)
-        return NULL;
-    memcpy(result, path1, len1);
-    memcpy(result + len1, path2, len2 + 1);
-
-    return result;
 }
--- src/logger.h
+++ src/logger.h
@@ -1,20 +1,27 @@
 #pragma once
 #include <stdio.h>
 
+int log_cleaned = 0;
+
 static inline void log_info(const char* log)
 {
     const char* log_type = "[Info] %s\n";
     printf(log_type, log);
-    FILE* file = fopen(".\\logs.txt", "a");
+    FILE* file = fopen(".\\logs.txt", log_cleaned == 1 ? "a" : "w");
+    log_cleaned = 1;
+
     fprintf(file, log_type, log);
     fclose(file);
 }
 
 static inline void log_warning(const char* log)
 {
+
     const char* log_type = "[Warning] %s\n";
     printf(log_type, log);
-    FILE* file = fopen(".\\logs.txt", "a");
+    FILE* file = fopen(".\\logs.txt", log_cleaned == 1 ? "a" : "w");
+    log_cleaned = 1;
+
     fprintf(file, log_type, log);
     fclose(file);
 }
@@ -23,7 +30,23 @@ static inline void log_error(const char* log)
 {
     const char* log_type = "[Error] %s\n";
     printf(log_type, log);
-    FILE* file = fopen(".\\logs.txt", "a");
+    FILE* file = fopen(".\\logs.txt", log_cleaned == 1 ? "a" : "w");
+    log_cleaned = 1;
+
     fprintf(file, log_type, log);
     fclose(file);
+}
+
+static inline char* concat(const char* s1, const char* s2)
+{
+    const size_t len1 = strlen(s1);
+    const size_t len2 = strlen(s2);
+
+    char* result = malloc(len1 + len2 + 1);
+    if (!result)
+        return NULL;
+    memcpy(result, s1, len1);
+    memcpy(result + len1, s2, len2 + 1);
+
+    return result;
 }
